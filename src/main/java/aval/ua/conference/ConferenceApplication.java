aed60f5568565c814499c4ece08be8141bcd064a
Roman <kiborg0@gmail.com>
2020-09-28 18:32:25 +0300

    Initial commit for conference project

---
+++ src/main/java/aval/ua/conference/ConferenceApplication.java
@@ -0,0 +1,13 @@
+package aval.ua.conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aval/ua/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package aval.ua.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
